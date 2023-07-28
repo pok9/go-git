a4ab9b6a701c832734cf9fd52143f85a2880c4f4
pok9 <pokhappy1999@gmail.com>
2023-07-28 22:35:07 +0700

    -feature :sparkles: add display

--- main.go
+++ main.go
@@ -10,8 +10,13 @@ func dev(a, b float64) float64 {
 	return a / b
 }
 
+func display(msg string) {
+	fmt.Println(msg)
+}
+
 func main() {
 	fmt.Println("Hello githib") // Hello githib
 	fmt.Println(sum(1, 2))      // 3
 	fmt.Println(dev(1, 2))      // 0.5
+	display("Hello Gitflow")
 }
