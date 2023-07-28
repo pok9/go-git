01975126d5600b4504e452f7934432090d64d387
pok9 <pokhappy1999@gmail.com>
2023-07-28 22:20:17 +0700

    -feture :sparkles: add dev

--- main.go
+++ main.go
@@ -5,7 +5,13 @@ import "fmt"
 func sum(a, b int) int {
 	return a + b
 }
+
+func dev(a, b float64) float64 {
+	return a / b
+}
+
 func main() {
-	fmt.Println("Hello githib") //Hello githib
-	fmt.Println(sum(1, 2))      //3
+	fmt.Println("Hello githib") // Hello githib
+	fmt.Println(sum(1, 2))      // 3
+	fmt.Println(dev(1, 2))      // 0.5
 }
